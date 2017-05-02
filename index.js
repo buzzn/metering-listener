2cf55bdc43d6ba974dcdb58a0374152a3cc3363e
Felix Faerber <ffaerber@gmail.com>
2017-05-02 19:14:02 +0200

    add timestamp

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 require('dotenv').config()
-
+var extend = require('util')._extend
 const mqtt = require('mqtt')
 const MongoClient = require('mongodb').MongoClient
-
+const time = require('time');
 const mqttClient = mqtt.connect(process.env.MQTT_BROKER_HOST)
 
 mqttClient.on('connect', function () {
@@ -14,7 +14,8 @@ MongoClient.connect(process.env.MONGODB_HOST, function(err, db) {
   console.log("Connected correctly to server");
   mqttClient.on('message', (topic, message) => {
     try {
-      let json = JSON.parse(message.toString());
+
+      let json = extend(JSON.parse(message.toString()),{timestamp: time.time()})
       insertDocuments(json, db, console.log);
     } catch (e) {
       console.log("not JSON");
@@ -24,7 +25,7 @@ MongoClient.connect(process.env.MONGODB_HOST, function(err, db) {
 
 
 let insertDocuments = (message, db, callback) => {
-  const collection = db.collection('readings');
+  const collection = db.collection(process.env.MONGODB_COLLECTION);
   collection.insertOne(message, (err, result) => {
     callback(result);
   });
